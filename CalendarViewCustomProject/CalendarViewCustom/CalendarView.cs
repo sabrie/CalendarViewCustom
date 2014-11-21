ebd7a5fd2c97c3440dea68b38101f7260d329edc
sabrie <sabrie.nedzhip@gmail.com>
2014-11-21 18:06:54 +0200

    Initial submit

---
+++ CalendarViewCustomProject/CalendarViewCustom/CalendarView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace CalendarViewCustom
+{
+    class CalendarView : Telerik.Sitefinity.Modules.Events.Web.UI.Public.CalendarView
+    {
+        protected override string ScriptDescriptorTypeName
+        {
+            get
+            {
+                return typeof(CalendarView).FullName;
+            }
+        }
+
+        public override IEnumerable<System.Web.UI.ScriptReference> GetScriptReferences()
+        {
+            return new List<ScriptReference>(base.GetScriptReferences())
+            {
+                new ScriptReference(this.scriptReference, typeof(CalendarView).Assembly.FullName)
+            };
+        }
+
+        public string scriptReference = "CalendarViewCustom.CalendarView.js";
+    }
+}
---
+++ CalendarViewCustomProject/CalendarViewCustom/CalendarView.js
@@ -0,0 +1,29 @@
+﻿Type.registerNamespace("CalendarViewCustom");
+
+CalendarViewCustom.CalendarView = function (element) {
+    CalendarViewCustom.CalendarView.initializeBase(this, [element]);
+}
+
+CalendarViewCustom.CalendarView.prototype = {
+    initialize: function () {
+        CalendarViewCustom.CalendarView.callBaseMethod(this, 'initialize');
+    },
+    dispose: function () {
+        CalendarViewCustom.CalendarView.callBaseMethod(this, 'dispose');
+    },
+
+    _onSchedulerAppointmentDataBoundHandler: function (sender, args) {
+        var calendarColor = this._getAttributeValueByKey(args.get_data().Attributes, "CalendarColor");
+        args.get_data()["IsEditable"] = this._getAttributeValueByKey(args.get_data().Attributes, "IsEditable");
+        var allDay = this._getAttributeValueByKey(args.get_data().Attributes, "AllDay");
+        if (calendarColor) {
+            args.get_appointment().set_backColor(calendarColor);
+        }
+    },
+    _onSchedulerRequestSuccessHandler: function (sender, args) {
+        this.set_startDST(args.get_result().StartDST);
+        this.set_endDST(args.get_result().EndDST);
+    }
+}
+
+CalendarViewCustom.CalendarView.registerClass('CalendarViewCustom.CalendarView', Telerik.Sitefinity.Modules.Events.Web.UI.Public.CalendarView);
